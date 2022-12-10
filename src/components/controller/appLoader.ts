29a6f10b9b175cfac9f9344555b320b42fc7abf0
MIKHAIL NOSOV <67101576+nos64@users.noreply.github.com>
2022-12-10 19:59:26 +0400

    refactor: update server

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -4,7 +4,8 @@ import { API_KEY } from '../../../KEY';
 class AppLoader extends Loader {
   constructor() {
     // super('https://newsapi.org/v2/', {
-    super('https://nodenews.herokuapp.com/', {
+    // super('https://nodenews.herokuapp.com/', {
+    super('https://newsapi-redirect-production.up.railway.app/', {
       apiKey: API_KEY,
     });
   }
